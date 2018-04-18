61158b602dfeadac94c58a0e38bd2875e328d14e
Marc Worrell <marc@worrell.nl>
2018-04-18 12:03:40 +0200

    Add copyright

--- test/router_tests.erl
+++ test/router_tests.erl
@@ -1,4 +1,16 @@
+%% @author Maas-Maarten <mmzeeman@xs4all.nl>
+%% @copyright 2015 Maas-Maarten Zeeman
 
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License
+%% at http://www.mozilla.org/MPL/
+%% 
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and
+%% limitations under the License.
+%%
 
 -module(router_tests).
 
